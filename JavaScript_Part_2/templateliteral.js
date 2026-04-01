ba5029e8744aec6da05caacb00631b15750b30a9
adityabhole165 <adityabhole0304@gmail.com>
2026-04-01 14:16:05 +0530

    template literal code

---
+++ JavaScript_Part_2/templateliteral.js
@@ -0,0 +1,8 @@
+let pencilPrice = 10;
+let eraserPrice = 5;
+let output = "the total price is :"+ ( pencilPrice + eraserPrice)+" "+"Rupees.";
+console.log(output);
+
+// other way
+let outputPencil = `The Pencil price is : ${pencilPrice} Rupees`;
+console.log(output);
