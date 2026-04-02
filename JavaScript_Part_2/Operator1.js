f09e16b919c75a722d786090a8b9c507bc7f852e
adityabhole165 <adityabhole0304@gmail.com>
2026-04-02 17:39:15 +0530

    Operator examples

---
+++ JavaScript_Part_2/Operator1.js
@@ -0,0 +1,25 @@
+//Arithmatic operators 
+console.log("Arithmatic operators");
+let a =10;
+let b = 5;
+console.log(a);
+console.log(b);
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a ** b);
+
+// Unary operators
+let m =10;
+let n = 5;
+console.log(m);
+console.log(n);
+
+console.log(m++);
+console.log(n--);
+
+console.log(m);
+console.log(n);
+
