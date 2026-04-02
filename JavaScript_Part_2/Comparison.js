8b7c2cd1b454e5ac8f90b9f30004524f55da9cd0
adityabhole165 <adityabhole0304@gmail.com>
2026-04-02 18:08:50 +0530

    comparison operator  in code

---
+++ JavaScript_Part_2/Comparison.js
@@ -0,0 +1,17 @@
+let age = 18;
+console.log(age >18); //false
+console.log(age =>18); //true
+console.log(age <18); //false
+age = 16
+console.log(age >18); 
+
+3 > 5
+// false
+5 > 6
+// false
+5 < 6
+// true
+5 >= 5
+// true
+0 < 5
+// true
