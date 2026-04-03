c51b7d0b9767f70cdaf9de8f5ae74ec74fa414ae
adityabhole165 <adityabhole0304@gmail.com>
2026-04-03 09:51:55 +0530

    if statement code

---
+++ JavaScript_Part_2/if_Statement.js
@@ -0,0 +1,28 @@
+// COnditional statement
+
+console.log("befor my if statment ");
+let age = 22;
+age = 14;
+age = 25;
+if (age >= 18){
+    console.log("You can vote ");
+    console.log("You can drive ");
+}
+age = 10;
+if (age < 18) {
+    console.log("you can not vote");
+    
+}
+age =20;
+if(age =>20){
+    console.log("you are in 20's");
+    
+}
+console.log("after my if statment ");
+
+
+let firstName = "Aditya";
+if(firstName == "Aditya"){
+    console.log(`welcome ${firstName}`);
+    
+}
