9a18a531d9f6604c1d97dd38254c1490305744e0
adityabhole165 <adityabhole0304@gmail.com>
2026-04-04 22:43:06 +0530

    else if examples

---
+++ JavaScript_Part_2/elseIf.js
@@ -0,0 +1,37 @@
+let age = 14;
+
+if (age >= 18) {
+    console.log("You can vote");
+}
+else if(age < 18) {
+    console.log("you can not vote");
+}
+else if(age <18){
+    console.log("you can not vote");
+}
+
+// 
+let marks = 75;
+
+if(marks >= 80){
+    console.log("A+");
+}
+else if (marks >=60) {
+    console.log("A");
+}
+else if (marks >=33) {
+    console.log("B");
+}
+else if(marks <32){
+    console.log("F");
+}
+
+// seasons
+let month = "january";
+if( month === 'january'){
+    console.log("winter  here");
+}
+else if(month === ' april'){
+    console.log("summer is here ");
+    
+}
