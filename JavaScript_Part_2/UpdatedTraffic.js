a62eefcf69f313fe521c65aa49197eeaf00126e2
adityabhole165 <adityabhole0304@gmail.com>
2026-04-04 22:43:31 +0530

    traffic signol example using else if

---
+++ JavaScript_Part_2/UpdatedTraffic.js
@@ -0,0 +1,11 @@
+let color;
+color = "red";
+if(color == "red"){
+    console.log("Stop");
+} 
+else if( color  == "yellow"  ){
+    console.log("Please see check and go slow down");
+ }
+else if(color == "green"){
+    console.log("Go");
+ }
