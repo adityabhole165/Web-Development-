253b4319696d1344bdedafa196e323f72d558018
adityabhole165 <adityabhole0304@gmail.com>
2026-04-04 23:01:43 +0530

    Practice Ques Solution

---
+++ JavaScript_Part_2/PracticeQueSol.js
@@ -0,0 +1,17 @@
+let size = 'A';
+
+if( size === 'XL'){
+    console.log(" Price is Rs.250");
+}
+else if(size === 'L'){
+    console.log("Price Rs 200");
+}
+else if(size === 'M'){
+    console.log("Price Rs 200");
+}
+else if(size === 'S' ){
+    console.log("price is rs 50");   
+}
+else {
+    console.log("size not available");   
+}
