32cfa0d11a9498bbd16235fabddd0eeeece70137
adityabhole165 <adityabhole0304@gmail.com>
2026-04-04 22:42:29 +0530

    Practice Question

---
+++ JavaScript_Part_2/PracticeQuesTraffic.js
@@ -0,0 +1,11 @@
+let color;
+color = "red";
+if(color == "red"){
+    console.log("Stop");
+} 
+ if( color  == "yellow"  ){
+    console.log("Please see check and go slow down");
+ }
+ if(color == "green"){
+    console.log("Go");
+ }
