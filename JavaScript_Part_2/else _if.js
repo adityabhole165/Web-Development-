62bb2268ec789784a218f3fa50740dc235b719b2
adityabhole165 <adityabhole0304@gmail.com>
2026-04-04 22:53:04 +0530

    else examples

---
+++ JavaScript_Part_2/else _if.js
@@ -0,0 +1,24 @@
+let age = 18;
+
+if(age < 18) {
+    console.log("You can vote");
+    
+}
+else{
+
+    console.log("You can not  vote");
+}
+
+
+let color = "red";
+
+if(color === "red"){
+    console.log("Stop");
+} else if( color === "green"){
+    console.log("GO");
+} else if(color ==="yellow"){
+    console.log("Slow down");
+} else {
+    console.log("Traffic light is broken");
+    
+}
