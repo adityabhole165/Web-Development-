c1134e552658da62eda690b91c76fabcaea8a77e
adityabhole165 <adityabhole0304@gmail.com>
2026-04-06 16:30:53 +0530

    switch case in js example

---
+++ JavaScript_Part_2/switch_example.js
@@ -0,0 +1,18 @@
+let color = "red";
+    switch(color) {
+        case "red":
+            console.log("stop");
+            break;
+        case "yellow" :
+            console.log("Slow down");
+            break;
+        case "green" :
+            console.log("Go");
+            break;
+        default :
+            console.log("Broken  Light");
+    
+    }
+
+    console.log("After switch statement");
+    
