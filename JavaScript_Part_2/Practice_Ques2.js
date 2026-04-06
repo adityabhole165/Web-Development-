793dd3fae3ee28a1e937b4436c34d916f0a17dfa
adityabhole165 <adityabhole0304@gmail.com>
2026-04-06 15:50:41 +0530

    Practice Ques Solution

---
+++ JavaScript_Part_2/Practice_Ques2.js
@@ -0,0 +1,25 @@
+// 
+// Q2 . Predict the output of following code
+// let num = 12 ;
+// if((num % 3 === 0 ) && (num+1 == 15) || (num - 1 == 11) ){
+//     console.log("safe");
+// } else {
+//     console.log("unsafe");
+// }
+//
+
+ let num = 12 ;
+if((num % 3 === 0 ) && (num+1 == 15) || (num - 1 == 11) ){
+    console.log("safe");
+} else {
+    console.log("unsafe");
+}
+
+
+// num % 3 === 0 :- true
+// num+1 == 15 := false
+// num - 1 == 11 :- true
+
+
+// flase || true = true
+// true && true := true
