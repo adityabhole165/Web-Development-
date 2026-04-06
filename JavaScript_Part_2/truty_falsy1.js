5be49cc4c76702f9ba04b9c4338d34f6c7dd2f28
adityabhole165 <adityabhole0304@gmail.com>
2026-04-06 16:13:47 +0530

    truty falsy example

---
+++ JavaScript_Part_2/truty_falsy1.js
@@ -0,0 +1,80 @@
+if(true){
+    console.log("it has true value");
+} else {
+    console.log("it has false value"); 
+}
+
+if(false){
+    console.log("it has true value");
+} else {
+    console.log("it has false value"); 
+}
+
+if(0){
+    console.log("it has true value");
+} else {
+    console.log("it has false value"); 
+}
+
+if(1){
+    console.log("it has true value");
+} else {
+    console.log("it has false value"); 
+}
+
+if(""){
+    console.log("it has true value");
+} else {
+    console.log("it has false value"); 
+}
+if("aditya"){
+    console.log("it has true value");
+} else {
+    console.log("it has false value"); 
+}
+if(null){
+    console.log("it has true value");
+} else {
+    console.log("it has false value"); 
+}
+if(undefined){
+    console.log("it has true value");
+} else {
+    console.log("it has false value"); 
+}
+
+let String = "";
+if(String){
+    console.log("it has not empty string value");
+} else {
+    console.log("it has String empty value"); 
+}
+
+let Str = "a";
+if(Str){
+    console.log("it has not empty string value");
+} else {
+    console.log("it has String empty value"); 
+}
+let Strs = " ";
+if(Strs){
+    console.log("it has not empty string value");
+} else {
+    console.log("it has String empty value"); 
+}
+
+let num =  0;
+if(num){
+    console.log("num is not equal to 0");
+} else {
+    console.log(" num is equal to 0");
+    
+}
+
+num =  -10;
+if(num){
+    console.log("num is not equal to 0");
+} else {
+    console.log(" num is equal to 0");
+    
+}
