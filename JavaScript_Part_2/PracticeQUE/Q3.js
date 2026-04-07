12c9ef243e169725f281a914eec0090e8acd41be
adityabhole165 <adityabhole0304@gmail.com>
2026-04-07 21:17:31 +0530

    Practice Question 3

---
+++ JavaScript_Part_2/PracticeQUE/Q3.js
@@ -0,0 +1,29 @@
+// Qs3. Write a switch statement to print the months in a quarter.
+// Months in Quarter 1 : January, February, March
+// Months in Quarter 2 : April, May, June
+// Months in Quarter 3 : July, August, September
+// Months in Quarter 4: October, November, December 
+
+let Quarter = 3;
+switch (Quarter) {
+    case 1:
+        console.log("January, February, March");
+        
+        break;
+    case 2:
+        console.log(" April, May, June");
+        
+        break;
+    case 3:
+        console.log("July, August, September");
+        
+        break;
+    case 4:
+        console.log("October, November, December");
+        break;
+    default:
+        console.log("Enter Queareted between 1 to 4");
+        
+        break;
+}
+
