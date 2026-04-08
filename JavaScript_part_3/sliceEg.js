d0c1bb36150589839eabfd5393628dc6f60aed5d
adityabhole165 <adityabhole0304@gmail.com>
2026-04-08 10:57:27 +0530

    slice example

---
+++ JavaScript_part_3/sliceEg.js
@@ -0,0 +1,10 @@
+let msg = "hello";
+console.log(msg.slice(0,4));
+
+let word = "college";
+console.log(word.slice(3,6));
+console.log(word.slice(-4));
+// college - len 7 it prints 7-4 it print lege
+
+
+
