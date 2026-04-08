930829fbb3d6af22fa71c4c9ce49b897ea39dd48
adityabhole165 <adityabhole0304@gmail.com>
2026-04-08 10:31:25 +0530

    method chaining example

---
+++ JavaScript_part_3/methodChain.js
@@ -0,0 +1,9 @@
+let msg = "     hello   ";
+console.log(msg);
+// let msg_new = msg.trim();
+// console.log("After trim ",msg_new);
+// msg_new = msg_new.toUpperCase();
+// console.log("After upper case ",msg_new);
+
+let newMsg = msg.trim().toUpperCase();
+console.log(newMsg);
