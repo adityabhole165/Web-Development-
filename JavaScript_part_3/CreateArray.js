2388a8d7f76fc673a2a6bec3dcc38e641bda6a89
adityabhole165 <adityabhole0304@gmail.com>
2026-04-08 14:11:55 +0530

    create array

---
+++ JavaScript_part_3/CreateArray.js
@@ -0,0 +1,11 @@
+let info = ["aditya",23,5.9];
+console.log(info.length);
+
+let emptyArr = [];
+console.log(emptyArr.length);
+
+console.log([1,2,3,4,5].length);
+
+console.log(info[0][0]);
+
+console.log(info[0].length);
