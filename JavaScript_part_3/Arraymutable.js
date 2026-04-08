ba11a23a4d785ac0e06d7e0d90c6fbfe99c06156
adityabhole165 <adityabhole0304@gmail.com>
2026-04-08 14:28:20 +0530

    mutable array

---
+++ JavaScript_part_3/Arraymutable.js
@@ -0,0 +1,12 @@
+
+let fruits = ["mango","apple","chiku"]
+console.log(fruits);
+fruits[2]="banana"
+console.log(fruits);
+fruits[4]="pineapple";
+console.log(fruits);
+fruits[10] = "papaya";
+console.log(fruits);
+console.log(fruits.length);
+
+
