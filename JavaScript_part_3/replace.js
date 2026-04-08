ed0d58fa1e8c217258019c6695de558cc1ad7c4a
adityabhole165 <adityabhole0304@gmail.com>
2026-04-08 13:06:53 +0530

    replace method eg

---
+++ JavaScript_part_3/replace.js
@@ -0,0 +1,11 @@
+let msg = "ILikeCoding";
+
+let newMsg = msg.replace("Like", "Do");
+console.log(newMsg);  // IDoCoding
+
+newMsg = msg.replace('o','x');
+console.log(newMsg);  
+
+msg = "runrunrun";
+newMsg = msg.replace('run','walk');
+console.log(newMsg);  
