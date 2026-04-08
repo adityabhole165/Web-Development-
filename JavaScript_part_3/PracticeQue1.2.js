98af99109ea489cfa8428924ea261956e9115b3f
adityabhole165 <adityabhole0304@gmail.com>
2026-04-08 13:34:50 +0530

    Practice Ques

---
+++ JavaScript_part_3/PracticeQue1.2.js
@@ -0,0 +1,7 @@
+let coursename = "ApnaCollege";
+console.log(coursename.slice(4,9));
+console.log(coursename.indexOf("na"));
+let newcourse = coursename.replace(4,9);
+console.log(newcourse);
+
+
---
+++ JavaScript_part_3/PracticeQue1.3.js
@@ -0,0 +1,10 @@
+let coursename ="apancollege"
+let updated = coursename.slice(4).replace("l","t");
+console.log(updated);
+
+let updated2 = updated.replace("l","t");
+console.log(updated2);
+
+let coursename2 ="apancollege"
+let courses = coursename2.slice(4).replace("l","t").replace("l","t");
+console.log(courses);
