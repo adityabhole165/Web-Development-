8ae5e9ff5f1708561a89cd95bdea83d458ba69a7
adityabhole165 <adityabhole0304@gmail.com>
2026-04-09 21:15:37 +0530

    Practice QUes

---
+++ JavaScript_part_3/practiceQueArr1.1.js
@@ -0,0 +1,9 @@
+// Qs. for the given start state of an array
+// change it to final form using splice
+// start = ['january','july','march','augast']
+// final = ['july','june','march','augast']
+let start = ['january','july','march','augast'];
+console.log(start.splice(0,2,"july","june"));
+console.log(start);
+
+
---
+++ JavaScript_part_3/practiceQueArr1.2.js
@@ -0,0 +1,8 @@
+//Return the index of the "javascript "
+// from the given array, if it was reversed
+// ['c','c++','html','javascript','python'
+// 'java','c#','sql']
+
+let arr =['c','c++','html','javascript','python','java','c#','sql'];
+console.log(arr.reverse().indexOf('javascript'));
+
