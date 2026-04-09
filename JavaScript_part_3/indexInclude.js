2524e8cfeaf5e57b503a7eca6f2a32e2f3dbb15c
adityabhole165 <adityabhole0304@gmail.com>
2026-04-09 21:11:50 +0530

    index of

---
+++ JavaScript_part_3/indexInclude.js
@@ -0,0 +1,10 @@
+let primary = ["red","yellow","blue"];
+console.log(primary.includes("red"));
+console.log(primary.includes("green"));
+console.log(primary.indexOf("green"));
+console.log(primary.indexOf("yellow"));
+
+let marks =[98,76,42,100,99];
+console.log(marks.indexOf(100));
+console.log(marks.indexOf(42));
+
