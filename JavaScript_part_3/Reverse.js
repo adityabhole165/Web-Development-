7c89e4f5e11338485c85393d8814286f8aa84fe0
adityabhole165 <adityabhole0304@gmail.com>
2026-04-09 21:13:46 +0530

    reverse

---
+++ JavaScript_part_3/Reverse.js
@@ -0,0 +1,11 @@
+// reverse - reverse an array
+
+let primary = ["red","yellow","blue"];
+
+console.log(primary.reverse());
+
+//output - [ 'blue', 'yellow', 'red' ]
+
+let cars = ["audi","bmw","xuv","pagani"];
+console.log(cars.reverse());
+// output 
