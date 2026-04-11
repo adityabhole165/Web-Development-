910041ab7e6a800038bf275cdef1d15de34560c0
adityabhole165 <adityabhole0304@gmail.com>
2026-04-11 12:09:58 +0530

    even nums 1 to 1000

--- JavaScript_part_4/evenNums.js
+++ JavaScript_part_4/evenNums.js
@@ -1,8 +1,12 @@
+// printing Even Numbers 
 for(let i=2; i<=10;i=i+2){
     console.log(i);
 }
 console.log("backward");
 
 for(let i=10; i>=2;i=i-2){
+    console.log(i);
+}
+for(let i=2; i<=1000;i=i+2){
     console.log(i);
 }
