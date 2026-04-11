9df39af9ba623b360fb627e0e642d8617ba5e54f
adityabhole165 <adityabhole0304@gmail.com>
2026-04-11 10:27:56 +0530

    Code without Using Modulo

---
+++ JavaScript_part_4/oddnums.js
@@ -0,0 +1,10 @@
+// print all odd numbers 1 to 15 
+let n= 20;
+for(let i= 1; i<=n;i=i+2){
+    console.log(i);
+}
+n=20;
+console.log("backwards");
+for(let i= 15; i>=1;i=i-2){
+    console.log(i);
+}
