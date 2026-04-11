462be767ac44e92aac1b0e39621bf64108e1f6ea
adityabhole165 <adityabhole0304@gmail.com>
2026-04-11 21:03:50 +0530

    nested for example

---
+++ JavaScript_part_4/nestedFor.js
@@ -0,0 +1,7 @@
+let Heros = [["Iron Man", "Captain America", "Thor"],["Wonder Woman", "BatMan", "Flash"]];
+for(hero of Heros){
+    // console.log(hero);
+    for(hero of Heros){
+        console.log(hero);   
+    }   
+}
