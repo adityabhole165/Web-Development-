e447ff7320aa3c08852c119de1e897666d8887f3
adityabhole165 <adityabhole0304@gmail.com>
2026-04-11 12:24:09 +0530

    multiplication table updated

--- JavaScript_part_4/Multiple_table.js
+++ JavaScript_part_4/Multiple_table.js
@@ -1,5 +1,11 @@
 // print multiplication table for 5
 let n = 5;
+
 for(let i = 1; i<=10; i++){
     console.log(n*i);
+}
+console.log("-----------------");
+
+for(let i = 5; i<=50; i=i+5){
+    console.log(i);
 }
