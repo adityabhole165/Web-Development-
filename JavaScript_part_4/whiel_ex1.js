50479f1fd9666a4c007f6c44bff37108a16b638d
adityabhole165 <adityabhole0304@gmail.com>
2026-04-11 21:02:26 +0530

    while loop

---
+++ JavaScript_part_4/whiel_ex1.js
@@ -0,0 +1,26 @@
+// let i = 1;
+// while(i <= 5) {
+//     console.log(i);
+//     i++;
+
+// }
+
+// let i= 1;
+// while (i <= 20) {
+//     console.log(i);
+//     i++;
+// }
+// let i= 1;
+// console.log("Odd nums");
+
+// while (i <= 20) {
+//     console.log(i);
+//     i=i+2;
+// }
+let i= 2;
+console.log("even nums ");
+
+while (i <= 20) {
+    console.log(i);
+    i=i+2;
+}
