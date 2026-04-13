fab800a8951a2178709da558a5a9b55421e8ec5c
adityabhole165 <adityabhole0304@gmail.com>
2026-04-13 11:29:05 +0530

    Object Literals

---
+++ JavaScript_part_5/ObjectLiterl_GetValues.js
@@ -0,0 +1,32 @@
+// Get Values 
+let student = {
+    name :"Aditya",
+    marks:94.4
+};
+
+student["name"];
+console.log(student["name"]);
+
+console.log(student.name);
+
+let post ={
+    username:"AdityaBhole003",
+    content:"This is my first post",
+    likes:200,
+    reposts:15,
+    tags:null
+};
+console.log(post["content"] );
+console.log(post.tags);
+console.log(post.username);
+console.log( post.reposts);
+console.log(post.content  );
+console.log(post["likes"]);
+
+
+let prop = "reposts";
+post[prop];
+console.log(post[prop]);
+
+
+
---
+++ JavaScript_part_5/Object_Literals.js
@@ -0,0 +1,27 @@
+// Object Literal 1
+const student = {
+    name:"aditya", //property 1
+    age:23,         //property 2
+    marks:94.4      //property 3
+};
+console.log(typeof student);
+console.log(student);
+
+let student2 = ["aditya",23,84.4]
+console.log(typeof student2);
+console.log(student2);
+
+
+// Object Literal 2
+let delhi = {
+    longitude:"28.7041 Degree North",
+    latitude: "77.1025 Degree East"
+};
+console.log(delhi);
+
+// example
+const item= {
+    price:100.99,
+    discount:50,
+    colors:["red","pink"]
+};
