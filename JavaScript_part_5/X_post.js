91b0b85ae2a05bba1d2173f01aebd06cf2d69a0e
adityabhole165 <adityabhole0304@gmail.com>
2026-04-13 11:28:44 +0530

    x_post

---
+++ JavaScript_part_5/X_post.js
@@ -0,0 +1,16 @@
+// Thread /Twitter post -
+// Create an Object lieral for the properties of thead / twitter post which include
+// Username
+// content
+// likes
+// reposts
+// tags
+
+let post ={
+    username:"AdityaBhole003",
+    content:"This is my first post",
+    likes:200,
+    reposts:15,
+    tags:null
+};
+console.log(post);
