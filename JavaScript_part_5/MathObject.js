a40e30c24207897553683c71f9d6efc8635927b7
adityabhole165 <adityabhole0304@gmail.com>
2026-04-15 07:21:46 +0530

    Math Object

---
+++ JavaScript_part_5/MathObject.js
@@ -0,0 +1,12 @@
+// Math Object
+// Properties Math.PI , Math.E
+// Methods - Math.abs(),Math.pow(a,b),Math.floor(n),Math.ceil(n),Math.random()
+// Math.abs() - gives nigative in positive
+// Math.pow(a,b) - a**b 
+// Math.floor(n) - give roundoff value of a number gives less than or equal value 
+// Nearest Smallest Integer Value
+
+// Math.ceil(n) - large number roundoff max value 
+
+// Math.random() - gives Random value 
+
