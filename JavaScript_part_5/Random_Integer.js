0e030d5529a9944322f5bb784689238ee3b349ef
adityabhole165 <adityabhole0304@gmail.com>
2026-04-15 07:21:59 +0530

    random Integer

---
+++ JavaScript_part_5/Random_Integer.js
@@ -0,0 +1,16 @@
+// Random Integer
+// From 1 to 10 
+// step 1: Let Num = Math.random();
+// step 2: num = num * 10;
+// step 3 : num = Math.floor(num);
+// step 4: num = num +1;
+
+let step1 = Math.random();
+console.log(step1);
+let step2 = step1 * 10;
+console.log(step2);
+let step3=  Math.floor(step2 );
+console.log(step3);
+console.log(step3+1);
+
+console.log(Math.floor(Math.random()*10)+1);
