d6d81cd4bd42922e7ba8e7da140bbcb3bfb155f5
adityabhole165 <adityabhole0304@gmail.com>
2026-04-15 11:26:51 +0530

    Practice Que

---
+++ JavaScript_part_5/PracticeQu.js
@@ -0,0 +1,6 @@
+// Qs. Generate a random number between 1 and 100
+let num = Math.floor(Math.random()*100)+1;
+console.log(num);
+// Qs.Generate a random number between 1 and 5
+let num1 = Math.floor(Math.random()*5)+1;
+console.log(num1);
