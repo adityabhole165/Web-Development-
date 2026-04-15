4b56e0fbd54ea569f92e82d02d300515b3adf494
adityabhole165 <adityabhole0304@gmail.com>
2026-04-15 07:21:29 +0530

    Nested Obj

---
+++ JavaScript_part_5/Nested_Objects.js
@@ -0,0 +1,18 @@
+// Object of Objects
+// Storing information of multiple Students 
+const classInfo ={
+    aman:{
+        grade:"A+",
+        city:"Delhi"
+    },
+    aditya:{
+        grade:"B+",
+        city:"Pune" 
+    },
+    karan:{
+        grade:"C",
+        city:"Mumbai"
+    }
+    
+}
+
