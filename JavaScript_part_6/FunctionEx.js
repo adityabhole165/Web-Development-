980b5094bbe22e209913e3626fbcfb5e23d9b360
adityabhole165 <adityabhole0304@gmail.com>
2026-04-16 22:17:49 +0530

    Function example

---
+++ JavaScript_part_6/FunctionEx.js
@@ -0,0 +1,34 @@
+function hellO(){
+    console.log("hello");
+}
+
+hellO();
+// Call Multiple times
+hellO();
+hellO();
+hellO();
+
+function PrintName() {
+    console.log("My name is Aditye");
+}
+
+PrintName();
+
+function Print1TO5() {
+    for(i =1; i<=6; i++){
+        console.log(i);
+        
+    }
+}
+
+Print1TO5();
+
+function CanDrive() {
+    let age = 22;
+    if(age > 22 ){
+        console.log("You can drive ");
+    } else {
+        console.log("No You are Not allow for drivinig ");
+        
+    }
+}
