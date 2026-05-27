20744be94588f5d013d3564afab70fb375f098ca
adityabhole165 <adityabhole0304@gmail.com>
2026-05-27 20:14:23 +0530

    function

--- JavaScript_part_6/Func_Args.js
+++ JavaScript_part_6/Func_Args.js
@@ -5,6 +5,11 @@
  *  // do somethings 
  * }
  */
+/**
+ * In functions Order is Important 
+ * when we pass the args as per passing order it stored in arg variables
+ *  as per varibles order defined pass values as per this 
+ */
 
 function printName(name) {
     console.log(name);  
@@ -17,3 +22,13 @@ function printInfo(name,age) {
 printName("Aditya")
 printInfo("Aditya")
 printInfo("Arjun",30)
+printInfo(30)
+
+function sum(a, b) {
+    console.log(a+b);
+}
+
+sum(1,2)
+sum(4,5)
+sum(7,6)
+sum(4555,78932)
---
+++ JavaScript_part_6/practics_Q3.js
@@ -0,0 +1,18 @@
+//Create a function that gives us the average of 3 numbers
+
+function calAvg(a,b,c){
+    let avg = (a+b+c)/3;
+    console.log(avg);
+}
+
+calAvg(2,4,6);
+
+Math.random
+
+
+[1,2,3].push
+[1,2,3].pop
+
+console.log
+
+sum
