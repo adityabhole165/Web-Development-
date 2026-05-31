2d160e3e775ab9c0874e252f9756b8ff0dd255a0
adityabhole165 <adityabhole0304@gmail.com>
2026-05-31 11:01:55 +0530

    scoper practice Ques

---
+++ JavaScript_part_6/practiceQ7.js
@@ -0,0 +1,18 @@
+// what will be the output 
+
+let greet = "hello" //global scope
+
+function changeGreet() {
+    let greet = "namaste"; //function scope
+    console.log(greet);
+
+    function innerGreen() {
+        console.log(greet); // lexical scope
+        
+    }
+    
+}
+
+console.log(greet);
+
+changeGreet();
