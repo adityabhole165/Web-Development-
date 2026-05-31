b9016901402f1d3e8edcf6fc9adb19b903bf64e7
adityabhole165 <adityabhole0304@gmail.com>
2026-05-31 12:01:33 +0530

    function expression in js

---
+++ JavaScript_part_6/funcExpr.js
@@ -0,0 +1,18 @@
+/**
+ * function Expressions 
+ *  const variable = function(arg1, arg2...) {
+ *              // do or return something 
+ *     }
+ */
+
+const sum = function(a,b){
+    return a + b;
+}
+
+console.log(sum(2,3));
+
+let hello = function() {
+    console.log("hello");
+    
+}
+hello()
